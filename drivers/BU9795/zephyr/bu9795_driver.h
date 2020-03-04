e5c1bec402f78ff60b9d8c2ba13c1e20fe1410aa
NZSmartie <nzsmartie@gmail.com>
2020-03-04 13:03:34 +1300

    Apply editorconfig rules across project

--- drivers/BU9795/zephyr/bu9795_driver.h
+++ drivers/BU9795/zephyr/bu9795_driver.h
@@ -13,23 +13,21 @@ extern "C" {
 #include <device.h>
 
 struct bu9795_driver_api {
-	void (*print)(struct device *dev);
+    void (*print)(struct device *dev);
 };
 
 __syscall     void        bu9795_print(struct device *dev);
 static inline void z_impl_bu9795_print(struct device *dev)
 {
-	const struct bu9795_driver_api *api = dev->driver_api;
+    const struct bu9795_driver_api *api = dev->driver_api;
 
-	__ASSERT(api->print, "Callback pointer should not be NULL");
+    __ASSERT(api->print, "Callback pointer should not be NULL");
 
-	api->print(dev);
+    api->print(dev);
 }
 
 #ifdef __cplusplus
 }
 #endif
-
-#include <syscalls/bu9795_driver.h>
 
 #endif /* __BU9795_DRIVER_H__ */
--- src/main.c
+++ src/main.c
@@ -12,40 +12,40 @@ static struct gpio_callback button_cb_data;
 
 void button_pressed(struct device *dev, struct gpio_callback *cb, u32_t pins)
 {
-	printk("Button pressed at %" PRIu32 "\n", k_cycle_get_32());
+    printk("Button pressed at %" PRIu32 "\n", k_cycle_get_32());
 }
 
 void main(void)
 {
-	struct device *dev_button;
-	int ret;
+    struct device *dev_button;
+    int ret;
 
-	printk("Hello world!\n");
+    printk("Hello world!\n");
 
-	dev_button = device_get_binding(DT_ALIAS_SW0_GPIOS_CONTROLLER);
-	if (dev_button == NULL) {
-		printk("Error: didn't find %s device\n", DT_ALIAS_SW0_GPIOS_CONTROLLER);
-		return;
-	}
+    dev_button = device_get_binding(DT_ALIAS_SW0_GPIOS_CONTROLLER);
+    if (dev_button == NULL) {
+        printk("Error: didn't find %s device\n", DT_ALIAS_SW0_GPIOS_CONTROLLER);
+        return;
+    }
 
-	ret = gpio_pin_configure(dev_button, DT_ALIAS_SW0_GPIOS_PIN, DT_ALIAS_SW0_GPIOS_FLAGS | GPIO_INPUT);
-	if (ret != 0) {
-		printk("Error %d: failed to configure pin %d '%s'\n", ret, DT_ALIAS_SW0_GPIOS_PIN, DT_ALIAS_SW0_LABEL);
-		return;
-	}
+    ret = gpio_pin_configure(dev_button, DT_ALIAS_SW0_GPIOS_PIN, DT_ALIAS_SW0_GPIOS_FLAGS | GPIO_INPUT);
+    if (ret != 0) {
+        printk("Error %d: failed to configure pin %d '%s'\n", ret, DT_ALIAS_SW0_GPIOS_PIN, DT_ALIAS_SW0_LABEL);
+        return;
+    }
 
-	ret = gpio_pin_interrupt_configure(dev_button, DT_ALIAS_SW0_GPIOS_PIN, GPIO_INT_EDGE_TO_ACTIVE);
-	if (ret != 0) {
-		printk("Error %d: failed to configure interrupt on pin %d '%s'\n", ret, DT_ALIAS_SW0_GPIOS_PIN, DT_ALIAS_SW0_LABEL);
-		return;
-	}
+    ret = gpio_pin_interrupt_configure(dev_button, DT_ALIAS_SW0_GPIOS_PIN, GPIO_INT_EDGE_TO_ACTIVE);
+    if (ret != 0) {
+        printk("Error %d: failed to configure interrupt on pin %d '%s'\n", ret, DT_ALIAS_SW0_GPIOS_PIN, DT_ALIAS_SW0_LABEL);
+        return;
+    }
 
-	gpio_init_callback(&button_cb_data, button_pressed, BIT(DT_ALIAS_SW0_GPIOS_PIN));
-	gpio_add_callback(dev_button, &button_cb_data);
+    gpio_init_callback(&button_cb_data, button_pressed, BIT(DT_ALIAS_SW0_GPIOS_PIN));
+    gpio_add_callback(dev_button, &button_cb_data);
 
-	printk("Press %s on the board\n", DT_ALIAS_SW0_LABEL);
+    printk("Press %s on the board\n", DT_ALIAS_SW0_LABEL);
 
-	while (1) {
-		k_sleep(1000);
-	}
+    while (1) {
+        k_sleep(1000);
+    }
 }
